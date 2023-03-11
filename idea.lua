8409d3f695b5e98d71056207779589d8e4ee4ec3
Nat Welch <nat@natwelch.com>
2023-03-11 17:08:33 +0000

    init

---
+++ idea.lua
@@ -0,0 +1,26 @@
+-- sketch: eucledia rythems for midi
+-- v1.0.0 @icco
+-- llllllll.co/t/22222
+
+engine.name = 'PolySub'
+norns.version.required = 221214
+
+function init()
+  -- initialization
+end
+
+function key(n,z)
+  -- key actions: n = number, z = state
+end
+
+function enc(n,d)
+  -- encoder actions: n = number, d = delta
+end
+
+function redraw()
+  -- screen redraw
+end
+
+function cleanup()
+  -- deinitialization
+end
